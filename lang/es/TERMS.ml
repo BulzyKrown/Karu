9080bd0b8b92f4247ceff1d1452936b404014f4d
BulzyKrown <darkmagiclost@gmail.com>
2022-09-13 23:03:01 -0300

    Create TERMS.ml

---
+++ lang/es/TERMS.ml
@@ -0,0 +1,13 @@
+# Términos y Condiciones
+
+- No se permite el mal uso del bot, en cuanto a abusar de un error no reportado, etc.
+
+- No hacer spam de comandos.
+
+- No compartir vulnerabilidades con fines maliciosos.
+
+- No se permiten actitudes negativas frente a otros usuarios a través del bot.
+
+El personal de Karu se reserva cualquier derecho de incluir usuarios o servidores en la lista negra como mejor lo vean necesario. También mencionar que los términos y condiciones estan sostenidos a modificaciones futuras las cuales podrian no ser notificadas.
+## Contacto
+`BulzyKrown#9999` en [Discord]([https://discord.gg/karu](https://discord.com/servers/karu-maid-season-410613670322634754)).
